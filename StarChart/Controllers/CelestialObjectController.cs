b291b475c360484dc886f7049ed290b6154257ae
Swapnil Mahajan <v-swma@microsoft.com>
2020-04-19 20:17:06 +0530

    commented to test

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -16,7 +16,7 @@ namespace StarChart.Controllers
 
         public CelestialObjectController(ApplicationDbContext context)
         {
-            _context = context;
+            //_context = context;
         }
     }
 }
