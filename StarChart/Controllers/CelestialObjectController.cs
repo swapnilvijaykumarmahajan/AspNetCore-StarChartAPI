d2612f9ba8ceeea4f1e015f6927c86227e909dfa
Swapnil Mahajan <v-swma@microsoft.com>
2020-04-20 11:11:07 +0530

    module 5 changes

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
@@ -17,6 +19,50 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var obj = _context.CelestialObjects.Where(x => x.Id == id).FirstOrDefault();
+            if (obj != null)
+            {
+                var orbitedObjects = _context.CelestialObjects.Where(x => x.OrbitedObjectId == id).ToList();
+                if (orbitedObjects != null && orbitedObjects.Any())
+                {
+                    obj.Satellites.AddRange(orbitedObjects);
+                }
+                return this.Ok(obj);
+            }
+            return this.NotFound();
+        }
+
+        [HttpGet("{name}", Name = "GetByName")]
+        public IActionResult GetByName(string name)
+        {
+            var obj = _context.CelestialObjects.Where(x => x.Name == name).FirstOrDefault();
+            if (obj != null)
+            {
+                var orbitedObjects = _context.CelestialObjects.Where(x => x.OrbitedObjectId == obj.Id).ToList();
+                if (orbitedObjects != null && orbitedObjects.Any())
+                {
+                    obj.Satellites.AddRange(orbitedObjects);
+                }
+                return this.Ok(obj);
+            }
+            return this.NotFound();
+        }
+        [HttpGet(Name = "GetAll")]
+        public IActionResult GetAll()
+        {
+            var allObjects = _context.CelestialObjects.ToList();
+            foreach (var obj in allObjects)
+            {
+                var satellites = allObjects.Where(x => x.OrbitedObjectId == obj.Id).ToList();
+                if (satellites != null && satellites.Any())
+                    obj.Satellites.AddRange(satellites);
+            }
+            return this.Ok(allObjects);
         }
     }
 }
--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -14,7 +14,7 @@ namespace StarChart.Models
         public string Name { get; set; }
         public int? OrbitedObjectId { get; set; }
         [NotMapped]
-        public List<CelestialObject> Satellites { get; set; }
+        public List<CelestialObject> Satellites { get; set; } = new List<CelestialObject>();
         public TimeSpan OrbitalPeriod { get; set; }
     }
 }
